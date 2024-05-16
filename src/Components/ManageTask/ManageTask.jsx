0a7ac494dec2a6bd0971eb2ac50c369c76e43f5e
abinayakathiresan <abinayakathiresan7@gmail.com>
2024-05-16 20:53:07 +0530

    added style

--- src/Components/ManageTask/ManageTask.jsx
+++ src/Components/ManageTask/ManageTask.jsx
@@ -1,9 +1,8 @@
-import { SettingsSystemDaydreamRounded } from "@mui/icons-material";
-
 import {
   Box,
   Button,
   Dialog,
+  DialogContent,
   DialogTitle,
   FormControl,
   InputLabel,
@@ -37,6 +36,10 @@ const ManageTask = () => {
   let editObj;
 
   const handleTask = () => {
+    if(!name){
+      alert("Task name is required");
+      return false;
+    }
     if (id) {
       let newList = tasks.map((item) => {
         return item.id == id
@@ -47,6 +50,7 @@ const ManageTask = () => {
     } else {
       if (tasks.length >= 5) {
         alert("You cant add more than 5 tasks");
+        handleCloseDialog();
         return;
       }
       const id = Math.random(9, 999);
@@ -61,6 +65,15 @@ const ManageTask = () => {
       copyTasks.push(taskObj);
       setTasks(copyTasks);
     }
+    handleCloseDialog();
+    resetForm();
+  };
+
+  const resetForm = () => {
+    setId("");
+    setName("");
+    setPriority("low");
+    setProgress("todo");
   };
 
   const handleEdit = (id) => {
@@ -82,137 +95,158 @@ const ManageTask = () => {
       setTasks(newtasks);
     }
   };
+
   const handleCloseDialog = () => {
     setOpenDialog(false);
   };
+
+  const handleOpenAdd = () => {
+    resetForm();
+    handleOpenDialog();
+  }
+
   const handleOpenDialog = () => {
     setOpenDialog(true);
   };
-  return (
-    <div>
-      <Dialog onClose={handleCloseDialog} open={openDialog}>
-        <DialogTitle>{id ? "Edit" : "Add"} Task Form</DialogTitle>
-        <div className="add-task__wrapper">
-          <div className="add-task__field">
-            <TextField
-              fullWidth
-              id="taskname"
-              label="Task Name"
-              variant="outlined"
-              value={name}
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-          </div>
-          <div className="add-task__field">
-            <FormControl fullWidth>
-              <InputLabel variant="standard" htmlFor="priority-native">
-                Priority
-              </InputLabel>
 
-              <NativeSelect
-                value={priority}
-                label="Priority"
-                inputProps={{
-                  name: "priority",
-                  id: "priority-native",
-                }}
-                onChange={(e) => {
-                  setPriority(e.target.value);
-                }}
-              >
-                <option value="low">Low</option>
-                <option value="medium">Medium</option>
-                <option value="high">High</option>
-              </NativeSelect>
-            </FormControl>
-          </div>
-          <div className="add-task__field">
-            <FormControl fullWidth>
-              <InputLabel variant="standard" htmlFor="progress-native">
-                Progress
-              </InputLabel>
-
-              <NativeSelect
-                value={progress}
-                label="progress"
-                inputProps={{
-                  name: "progress",
-                  id: "progress-native",
-                }}
-                onChange={(e) => {
-                  setProgress(e.target.value);
-                }}
-              >
-                <option value="todo">To Do</option>
-                <option value="inprogress">In Progress</option>
-                <option value="done">Done</option>
-              </NativeSelect>
-            </FormControl>
-          </div>
-          <div className="add-task__field">
-            <Button variant="contained" onClick={handleTask}>
-              Submit
-            </Button>
-          </div>
+  const renderTaskForm = () => {
+    return (<Dialog onClose={handleCloseDialog} open={openDialog}>
+      <DialogTitle>{id ? "Edit" : "Add"} Task Form</DialogTitle>
+      <DialogContent>
+      <div className="add-task__wrapper">
+        <div className="add-task__field">
+          <TextField
+            fullWidth
+            id="taskname"
+            label="Task Name"
+            variant="outlined"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
         </div>
-      </Dialog>
+        <div className="add-task__field">
+          <FormControl fullWidth>
+            <InputLabel variant="standard" htmlFor="priority-native">
+              Priority
+            </InputLabel>
 
-      <div className="row">
-        <Typography variant="h4">Task List</Typography>
-        <div>
-          <Button variant="contained" onClick={handleOpenDialog}>
-            Add Task
+            <NativeSelect
+              value={priority}
+              label="Priority"
+              inputProps={{
+                name: "priority",
+                id: "priority-native",
+              }}
+              onChange={(e) => {
+                setPriority(e.target.value);
+              }}
+            >
+              <option value="low">Low</option>
+              <option value="medium">Medium</option>
+              <option value="high">High</option>
+            </NativeSelect>
+          </FormControl>
+        </div>
+        <div className="add-task__field">
+          <FormControl fullWidth>
+            <InputLabel variant="standard" htmlFor="progress-native">
+              Progress
+            </InputLabel>
+
+            <NativeSelect
+              value={progress}
+              label="progress"
+              inputProps={{
+                name: "progress",
+                id: "progress-native",
+              }}
+              onChange={(e) => {
+                setProgress(e.target.value);
+              }}
+            >
+              <option value="todo">To Do</option>
+              <option value="inprogress">In Progress</option>
+              <option value="done">Done</option>
+            </NativeSelect>
+          </FormControl>
+        </div>
+        <div className="add-task__field">
+          <Button variant="contained" onClick={handleTask}>
+            Submit
           </Button>
         </div>
       </div>
+      </DialogContent>
+    </Dialog>)
+  }
 
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Task Name</TableCell>
-              <TableCell align="right">Priority</TableCell>
-              <TableCell align="right">Progress</TableCell>
-              <TableCell align="right">Actions</TableCell>
+  const renderTaskList = () => {
+    return (<TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Task Name</TableCell>
+            <TableCell align="left">Priority</TableCell>
+            <TableCell align="left">Progress</TableCell>
+            <TableCell align="left">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {tasks.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="left">{row.priority}</TableCell>
+              <TableCell align="left">{row.progress}</TableCell>
+              <TableCell align="left">
+                <div className="task-list__actions">
+                <Button
+                variant="outlined"
+                  onClick={(e) => {
+                    handleEdit(row.id);
+                  }}
+                >
+                  Edit
+                </Button>
+                <Button
+                 variant="outlined"
+                  onClick={(e) => {
+                    handleDelete(row.id);
+                  }}
+                >
+                  Delete
+                </Button>
+                </div>
+              </TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {tasks.map((row) => (
-              <TableRow
-                key={row.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.priority}</TableCell>
-                <TableCell align="right">{row.progress}</TableCell>
-                <TableCell align="right">
-                  <Button
-                    onClick={(e) => {
-                      handleEdit(row.id);
-                    }}
-                  >
-                    Edit
-                  </Button>
-                  <Button
-                    onClick={(e) => {
-                      handleDelete(row.id);
-                    }}
-                  >
-                    Delete
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>);
+  }
+  return (
+    <div class="task">
+      {renderTaskForm()}
+
+      <div className="task-list__header">
+        <Typography variant="h4">Task List</Typography>
+        <Button variant="contained" onClick={handleOpenAdd}>
+          Add Task
+        </Button>
+      </div>
+
+      {renderTaskList()}
+
+      
     </div>
   );
 };
